b4ca0638f37bf6fc2a6cc34cdb11926fc0c42373
Wagner Junior <wagnerjunior@lett.digital>
2021-01-27 20:16:14 -0300

    feat: add login page

---
+++ src/main/index.ts
@@ -0,0 +1,4 @@
+import { createApp } from "vue";
+import Login from "@/presentation/pages/login/login.vue";
+
+createApp(Login).mount("#main");
---
+++ src/presentation/components/spinner/spinner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="spinner">
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Spinner",
+});
+</script>
+
+<style lang="scss">
+@import "./spinner-styles.scss";
+</style>
