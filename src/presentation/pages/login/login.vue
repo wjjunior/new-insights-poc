fe338f5dac4a7b3ec52704655f226d9af6d7325e
Wagner Junior <wagnerjunior@lett.digital>
2021-01-29 16:56:17 -0300

    feat: ensure Login starts with submit button disabled

--- src/presentation/pages/login/login.vue
+++ src/presentation/pages/login/login.vue
@@ -6,7 +6,7 @@
         <h2>Login</h2>
         <Input type="email" name="email" placeholder="Digite seu e-mail" />
         <Input type="password" name="password" placeholder="Digite sua senha" />
-        <button type="button" class="submit" @click="login">Entrar</button>
+        <button data-testid="submit" disabled type="button" class="submit" @click="login">Entrar</button>
         <span class="link">Criar conta</span>
         <FormStatus />
       </form>
