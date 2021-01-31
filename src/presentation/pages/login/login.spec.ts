c1186c4bfe24bb04223661774a566b2a432b795d
Wagner Junior <wagnerjunior@lett.digital>
2021-01-31 02:00:18 -0300

    refactor: change validationStub to initialize without error

--- src/presentation/pages/login/login.spec.ts
+++ src/presentation/pages/login/login.spec.ts
@@ -7,12 +7,15 @@ import { ComponentPublicInstance } from 'vue'
 
 type SutTypes = {
   sut: VueWrapper<ComponentPublicInstance>;
-  validationStub: ValidationStub;
 };
 
-const makeSut = (): SutTypes => {
+type SutParams = {
+  validationError: string
+}
+
+const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
-  validationStub.errorMessage = faker.random.words()
+  validationStub.errorMessage = params?.validationError
   const sut = mount(Login, {
     props: {
       validation: validationStub
@@ -22,48 +25,49 @@ const makeSut = (): SutTypes => {
     }
   })
   return {
-    sut,
-    validationStub
+    sut
   }
 }
 
 describe('Login Component', () => {
   test('Should start with initial state', () => {
-    const { sut, validationStub } = makeSut()
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
     const errorWrap = sut.get('[data-test="error-wrap"]')
     expect(errorWrap.element.childElementCount).toBe(0)
     const submitButton = sut.get('[data-test="submit"]')
       .element as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
     const emailStatus = sut.get('[data-test="email-status"]')
-    expect(emailStatus.attributes('title')).toBe(validationStub.errorMessage)
+    expect(emailStatus.attributes('title')).toBe(validationError)
     expect(emailStatus.element.textContent).toBe('🔴')
     const passwordStatus = sut.get('[data-test="password-status"]')
-    expect(passwordStatus.attributes('title')).toBe(validationStub.errorMessage)
+    expect(passwordStatus.attributes('title')).toBe(validationError)
     expect(passwordStatus.element.textContent).toBe('🔴')
   })
 
   test('Should show email error if Validation fails', async () => {
-    const { sut, validationStub } = makeSut()
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
     sut.setData({ email: faker.internet.email() })
     await sut.vm.$nextTick()
     const emailStatus = sut.get('[data-test="email-status"]')
-    expect(emailStatus.attributes('title')).toBe(validationStub.errorMessage)
+    expect(emailStatus.attributes('title')).toBe(validationError)
     expect(emailStatus.element.textContent).toBe('🔴')
   })
 
   test('Should show password error if Validation fails', async () => {
-    const { sut, validationStub } = makeSut()
+    const validationError = faker.random.words()
+    const { sut } = makeSut({ validationError })
     sut.setData({ password: faker.internet.password() })
     await sut.vm.$nextTick()
     const passwordStatus = sut.get('[data-test="password-status"]')
-    expect(passwordStatus.attributes('title')).toBe(validationStub.errorMessage)
+    expect(passwordStatus.attributes('title')).toBe(validationError)
     expect(passwordStatus.element.textContent).toBe('🔴')
   })
 
   test('Should show valid email state if Validation success', async () => {
-    const { sut, validationStub } = makeSut()
-    validationStub.errorMessage = null
+    const { sut } = makeSut()
     sut.setData({ email: faker.internet.email() })
     await sut.vm.$nextTick()
     const emailStatus = sut.get('[data-test="email-status"]')
@@ -72,8 +76,7 @@ describe('Login Component', () => {
   })
 
   test('Should show valid password state if Validation success', async () => {
-    const { sut, validationStub } = makeSut()
-    validationStub.errorMessage = null
+    const { sut } = makeSut()
     sut.setData({ password: faker.internet.password() })
     await sut.vm.$nextTick()
     const passwordStatus = sut.get('[data-test="password-status"]')
@@ -82,8 +85,7 @@ describe('Login Component', () => {
   })
 
   test('Should enable submit button if form is valid', async () => {
-    const { sut, validationStub } = makeSut()
-    validationStub.errorMessage = null
+    const { sut } = makeSut()
     sut.setData({ email: faker.internet.email() })
     sut.setData({ password: faker.internet.password() })
     await sut.vm.$nextTick()
