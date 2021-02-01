2ed527f30d68e988001b10822813c3bba3c43c2f
Wagner Junior <wagnerjunior@lett.digital>
2021-02-01 18:19:48 -0300

    chore: add .env

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,70 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { VueLoaderPlugin } = require("vue-loader");
+const dotenv = require('dotenv')
+const { DefinePlugin, ProvidePlugin } = require('webpack')
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main/index.ts",
+  mode: 'development',
+  entry: './src/main/index.ts',
   output: {
-    path: path.join(__dirname, "public/js"),
-    publicPath: "/public/js",
-    filename: "bundle.js",
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: [".ts", ".js", ".vue", ".scss"],
+    extensions: ['.ts', '.js', '.vue', '.scss'],
     alias: {
-      "@": path.join(__dirname, "src"),
-    },
+      '@': path.join(__dirname, 'src'),
+      process: 'process/browser'
+    }
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
         options: {
-          appendTsSuffixTo: [/\.vue$/],
-        },
+          appendTsSuffixTo: [/\.vue$/]
+        }
       },
       {
         test: /\.vue$/,
-        loader: "vue-loader",
+        loader: 'vue-loader'
       },
       {
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader'
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader'
           },
           {
-            loader: "sass-loader",
-          },
-        ],
-      },
-    ],
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   devServer: {
-    contentBase: "./public",
+    contentBase: './public',
     writeToDisk: true,
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   externals: {
-    vue: "Vue",
+    vue: 'Vue'
   },
-  plugins: [new CleanWebpackPlugin(), new VueLoaderPlugin()],
-};
+  plugins: [
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+    new ProvidePlugin({
+      process: 'process/browser'
+    }),
+    new DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed)
+    })
+  ]
+}
