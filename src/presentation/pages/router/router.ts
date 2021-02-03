1097717fca1babb12a4fe9eda1b0bffa06c1773b
Wagner Junior <wagnerjunior@lett.digital>
2021-02-02 23:27:25 -0300

    fix: fix router start point

--- src/presentation/pages/router/router.ts
+++ src/presentation/pages/router/router.ts
@@ -1,6 +1,6 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import { makeLogin } from '@/main/factories/pages'
-import { store } from '@/presentation/store'
+// import { store } from '@/presentation/store'
 import App from '@/main/App.vue'
 
 const routes = [
@@ -24,16 +24,16 @@ const router = createRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.allowAnonymous)) {
-    if (store.getters.isAuthenticated) {
-      next()
-      return
-    }
-    next('/login')
-  } else {
-    next()
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some(record => record.meta.allowAnonymous)) {
+//     if (store.getters.isAuthenticated) {
+//       next()
+//       return
+//     }
+//     next('/login')
+//   } else {
+//     next()
+//   }
+// })
 
 export default router
